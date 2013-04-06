c0c15379e04d6d3b910ebcf57569c6ecdb956c59
Alexandru Onea <onea.alex@gmail.com>
2013-04-06 16:16:45 +0100

    added toggle bass/treble clef support

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,11 @@
 var clef = document.querySelector('#clef');
 var title = document.querySelector('.title');
 
+var treble_select = document.querySelector('.treble-select');
+var bass_select = document.querySelector('.bass-select');
+
+var current_clef = $('.current-clef');
+
 var onPageLoad = function () {
 	// Init
 }
@@ -29,5 +34,34 @@ var titleKeyEventHandle = function (e) {
 
 /* Click handles */
 
+var toggleClefSelectDropdown = function (e) {
+	var menu = document.querySelector('.clef-select-menu');
+	menu.style.top = e.y + 60 + 'px';
+	menu.style.left = e.x - 20 + 'px';
+}
+
+var selectTreble = function (e) {
+	current_clef.fadeOut(300, function () {
+		current_clef.removeClass('bass').addClass('treble');;
+		current_clef.fadeIn(300);
+	});
+	e.preventDefault();
+}
+
+var selectBass = function (e) {
+	current_clef.fadeOut(300, function () {
+		current_clef.removeClass('treble').addClass('bass');;
+		current_clef.fadeIn(300);
+	});
+	e.preventDefault();
+}
+
+$('#clef').click(function() {
+        $(this).next('.dropdown-menu').fadeToggle(200);
+    });
+
 window.onload = onPageLoad;
 title.onkeydown = titleKeyEventHandle;
+clef.onclick = toggleClefSelectDropdown;
+bass_select.onclick = selectBass;
+treble_select.onclick = selectTreble;
